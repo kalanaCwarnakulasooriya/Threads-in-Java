b46623a1f089818ce860c33cc641dbd5a173ffcf
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-04-28 23:11:32 +0530

    06-Thread-Priority

---
+++ 06-Thread-Priority/src/main/java/lk/ijse/Main.java
@@ -0,0 +1,41 @@
+package lk.ijse;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("A : " + Thread.currentThread().getPriority());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("B : " + Thread.currentThread().getPriority());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        thread1.start();
+        thread1.setPriority(Thread.MAX_PRIORITY);
+        thread2.start();
+        thread2.setPriority(Thread.MIN_PRIORITY);
+
+        //max-10
+        //min-1
+        //normal-5
+
+        System.out.println("T1 : " + thread1.getPriority());
+        System.out.println("T2 : " + thread2.getPriority());
+    }
+}

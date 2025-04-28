dda075f700c0d8c5d3989733fae863dc8f815324
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-04-28 21:15:17 +0530

    01-Introduction

---
+++ 01-Introduction/src/main/java/lk/ijse/ExtendThreadClass.java
@@ -0,0 +1,15 @@
+package lk.ijse;
+
+public class ExtendThreadClass {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Thread is running");
+    }
+}
